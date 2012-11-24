85114f5e6625371f9c5aa55d099b395887d5d2e1
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-11-25 00:03:39 +0400

    seeds file was added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+FactoryGirl.create(:region, :name => 'Российская Федерация', :default => true)
+
+acols = [] 
+5.times { acols << FactoryGirl.create(:acol) }
+
+acols.each do |acol|
+  AdvocatoryFormation.get_form_values.map do |form, form_name|
+    formation = FactoryGirl.create(:advocatory_formation, :form => form)
+    number_of_advocates = 10
+    if (formation.form == :cabinet)
+      number_of_advocates = 1
+    end
+    number_of_advocates.times { formation.advocates << FactoryGirl.build(:advocate, :acol => acol) }
+  end
+end
