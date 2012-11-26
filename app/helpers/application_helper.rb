d9829088e2e327d4127f42305ec6ffaa3a2ad88a
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-11-26 22:56:35 +0400

    Changed the colour for notice alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,7 @@ module ApplicationHelper
   
   @@flash_classes =
     {:notice  => 'alert-info',
-     :alert   => 'alert-error',
-     :success => 'alert-success'}
+     :alert   => 'alert-error'}
 
   def flash_class_for(key)
     @@flash_classes[key]
